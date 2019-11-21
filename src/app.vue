43d1811eb6120b641164c0c057f7255c0d32110c
Cedric Hobbs <cedric@anwansi.com>
2019-11-20 23:49:42 -0800

    Update and massively expand sample data

--- src/app.vue
+++ src/app.vue
@@ -34,14 +34,20 @@ export default {
                     format : { stringTitle : true }
                 },
                 {
-                    id     : 'country',
-                    name   : 'Country',
+                    id     : 'trim',
+                    name   : 'Trim',
                     type   : 'string',
                     format : { stringUpper : true }
                 },
                 {
-                    id     : 'owned',
-                    name   : 'Owned',
+                    id     : 'mileage',
+                    name   : 'Mileage',
+                    type   : 'number',
+                    format : { numberInteger : true, numberFormat : true }
+                },
+                {
+                    id     : 'domestic',
+                    name   : 'Domestic',
                     type   : 'boolean',
                     format : {
                         booleanTextMap : { 'true' : 'Yes', 'false' : 'No' }
@@ -50,18 +56,132 @@ export default {
             ],
             rows : [
                 {
-                    year    : { value : 1980 },
-                    make    : { value : 'chevrolet' },
-                    model   : { value : 'caprice classic' },
-                    country : { value : 'usa' },
-                    owned   : { value : true }
+                    year     : { value : 1986 },
+                    make     : { value : 'chevrolet' },
+                    model    : { value : 'caprice classic' },
+                    trim     : { value : '', displayValue : '' },
+                    mileage  : { value : 172191 },
+                    domestic : { value : true }
                 },
                 {
-                    year    : { value : 1989 },
-                    make    : { value : 'pontiac' },
-                    model   : { value : 'firebird' },
-                    country : { value : 'uk' },
-                    owned   : { value : false }
+                    year     : { value : 1989 },
+                    make     : { value : 'pontiac' },
+                    model    : { value : 'trans am' },
+                    trim     : { value : 'gta' },
+                    mileage  : { value : 119774 },
+                    domestic : { value : true }
+                },
+                {
+                    year     : { value : 1990 },
+                    make     : { value : 'honda' },
+                    model    : { value : 'accord' },
+                    trim     : { value : 'dx' },
+                    mileage  : { value : 98394 },
+                    domestic : { value : false }
+                },
+                {
+                    year     : { value : 2004 },
+                    make     : { value : 'ford' },
+                    model    : { value : 'f-150' },
+                    trim     : { value : 'xlt' },
+                    mileage  : { value : 57510 },
+                    domestic : { value : true }
+                },
+                {
+                    year     : { value : 1983 },
+                    make     : { value : 'volkswagen' },
+                    model    : { value : 'rabbit' },
+                    trim     : { value : '', displayValue : '' },
+                    mileage  : { value : 198987 },
+                    domestic : { value : false }
+                },
+                {
+                    year     : { value : 2008 },
+                    make     : { value : 'mercury' },
+                    model    : { value : 'sable' },
+                    trim     : { value : 'ls' },
+                    mileage  : { value : 70481 },
+                    domestic : { value : true }
+                },
+                {
+                    year     : { value : 1988 },
+                    make     : { value : 'nissan' },
+                    model    : { value : 'pulsar' },
+                    trim     : { value : 'nx' },
+                    mileage  : { value : 151312 },
+                    domestic : { value : false }
+                },
+                {
+                    year     : { value : 1982 },
+                    make     : { value : 'oldsmobile' },
+                    model    : { value : 'delta 88' },
+                    trim     : { value : 'royale', displayValue : 'Royale' },
+                    mileage  : { value : 216673 },
+                    domestic : { value : false }
+                },
+                {
+                    year     : { value : 2001 },
+                    make     : { value : 'toyota' },
+                    model    : { value : 'camry' },
+                    trim     : { value : 'xle' },
+                    mileage  : { value : 83306 },
+                    domestic : { value : false }
+                },
+                {
+                    year     : { value : 1991 },
+                    make     : { value : 'buick' },
+                    model    : { value : 'century' },
+                    trim     : { value : '', displayValue : '' },
+                    mileage  : { value : 124991 },
+                    domestic : { value : true }
+                },
+                {
+                    year     : { value : 1993 },
+                    make     : { value : 'cadillac' },
+                    model    : { value : 'fleetwood' },
+                    trim     : { value : '', displayValue : '' },
+                    mileage  : { value : 97772 },
+                    domestic : { value : true }
+                },
+                {
+                    year     : { value : 2003 },
+                    make     : { value : 'lincoln' },
+                    model    : { value : 'town car' },
+                    trim     : { value : 'signature series', displayValue : 'Signature Series' },
+                    mileage  : { value : 60138 },
+                    domestic : { value : true }
+                },
+                {
+                    year     : { value : 1988 },
+                    make     : { value : 'mazda' },
+                    model    : { value : '323' },
+                    trim     : { value : 'gt-x' },
+                    mileage  : { value : 101188 },
+                    domestic : { value : false }
+                },
+                {
+                    year     : { value : 1999 },
+                    make     : { value : 'gmc', displayValue : 'GMC' },
+                    model    : { value : 'yukon' },
+                    trim     : { value : 'slt' },
+                    mileage  : { value : 235006 },
+                    domestic : { value : true }
+                },
+                {
+                    year     : { value : 1987 },
+                    make     : { value : 'mercedes' },
+                    model    : { value : '500sel', displayValue : '500SEL' },
+                    trim     : { value : '', displayValue : '' },
+                    mileage  : { value : 16299 },
+                    domestic : { value : false }
+                },
+                {
+                    year     : { value : 2000 },
+                    make     : { value : 'audi' },
+                    model    : { value : 'tt', displayValue : 'TT' },
+                    trim     : { value : '', displayValue : '' },
+                    mileage  : { value : 89092 },
+                    domestic : { value : false }
                 }
             ]
         };
